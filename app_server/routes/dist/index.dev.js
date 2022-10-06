048b94050f083afa04b8e9084aa99821350ad82c
Şevval Tuncay <sevvaltuncy@gmail.com>
2022-10-06 15:02:07 +0300

    first commit

---
+++ app_server/routes/dist/index.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+
+var ctrlMain = require("../controllers/main");
+
+router.get('/', ctrlMain.index);
+module.exports = router;
+//# sourceMappingURL=index.dev.js.map
