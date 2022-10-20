36d3d1f90bd381818bc1fba79bc727e951164356
Şevval Tuncay <sevvaltuncy@gmail.com>
2022-10-20 18:57:12 +0300

    third commit

--- app_server/controllers/dist/mekanlar.dev.js
+++ app_server/controllers/dist/mekanlar.dev.js
@@ -1,18 +1,65 @@
 "use strict";
 
-var express = require('express');
-
-var router = express.Router();
-
-var anasayfa = function anasayfa(req, res, next) {
+var anasayfa = function anasayfa(req, res) {
   res.render('anasayfa', {
-    'title': "Anasayfa"
+    "baslik": "Anasayfa",
+    "sayfaBaslik": {
+      "siteAd": "MekanBul",
+      "slogan": "Civardaki Mekanları Keşfet!"
+    },
+    "mekanlar": [{
+      "ad": "Starbucks",
+      "puan": "3",
+      "adres": "Iyaspark AVM",
+      "imkanlar": ["Kahveler", "Tatlılar", "Dünya Kahveleri"],
+      "mesafe": "3km"
+    }, {
+      "ad": "Gloria Jeans Coffee",
+      "puan": "2",
+      "adres": "SDU Dogu Yerleskesi",
+      "imkanlar": ["Kahveler", "İçecekler"],
+      "mesafe": "5km"
+    }, {
+      "ad": "Cam Kafe",
+      "puan": "4",
+      "adres": "Sdu Batı yerleskesi",
+      "imkanlar": ["Tost", "Çay"],
+      "mesafe": "10km"
+    }]
   });
 };
 
 var mekanbilgisi = function mekanbilgisi(req, res) {
   res.render('mekanbilgisi', {
-    'title': "Mekan Bilgisi"
+    "baslik": "Mekan Bilgisi",
+    "mekanBaslik": "Starbucks",
+    "mekanDetay": {
+      "ad": "Starbucks",
+      "puan": "3",
+      "adres": "Iyaspark AVM",
+      "imkanlar": ["Kahveler", "Tatlılar", "Dünya Kahveleri"],
+      "saatler": [{
+        "gunler": "Pazartesi-Cuma",
+        "acilis": "9:00",
+        "kapanis": "23:00",
+        "kapali": false
+      }, {
+        "gunler": "Cumartesi-Pazar",
+        "acilis": "10:00",
+        "kapanis": "22:00",
+        "kapali": false
+      }],
+      "koordinatlar": {
+        "enlem": "37.78334",
+        "boylam": "30.54530"
+      },
+      "yorumlar": [{
+        "yorumYapan": "Şevval Tuncay",
+        "puan": "3",
+        "tarih": "20 Ekim 2022",
+        "yorumMetni": "Kahveleri ve pastaları güzel"
+      }]
+    }
   });
 };
 
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,14 +1,78 @@
-var express = require('express');
-var router = express.Router();
-
-
-
-const anasayfa = function(req, res, next){
-    res.render('anasayfa', { 'title': "Anasayfa" });
+const anasayfa = function (req, res){
+    res.render('anasayfa', { 
+        "baslik": "Anasayfa" ,
+        "sayfaBaslik": {
+            "siteAd": "MekanBul",
+            "slogan": "Civardaki Mekanları Keşfet!"
+        },
+        "mekanlar": [
+            {
+                "ad": "Starbucks",
+                "puan": "3",
+                "adres": "Iyaspark AVM",
+                "imkanlar": ["Kahveler","Tatlılar","Dünya Kahveleri"],
+                "mesafe" : "3km"
+        
+            }
+            ,
+            {
+                "ad": "Gloria Jeans Coffee",
+                "puan": "2",
+                "adres": "SDU Dogu Yerleskesi",
+                "imkanlar": ["Kahveler","İçecekler"],
+                "mesafe" : "5km"
+        
+            }
+            ,
+            {
+                "ad": "Cam Kafe",
+                "puan": "4",
+                "adres": "Sdu Batı yerleskesi",
+                "imkanlar": ["Tost","Çay"],
+                "mesafe" : "10km"
+            }
+        ]
+    }
+    );
 }
-
 const mekanbilgisi = function(req, res){
-    res.render('mekanbilgisi', { 'title': "Mekan Bilgisi" });
+    res.render('mekanbilgisi', 
+    {  "baslik": "Mekan Bilgisi",
+       "mekanBaslik": "Starbucks",
+       "mekanDetay": {
+            "ad": "Starbucks",
+            "puan":"3",
+            "adres": "Iyaspark AVM",
+            "imkanlar": ["Kahveler", "Tatlılar", "Dünya Kahveleri"],
+            "saatler":[
+                {
+                    "gunler":"Pazartesi-Cuma",
+                    "acilis": "9:00",
+                    "kapanis": "23:00",
+                    "kapali": false
+                },
+                {
+                    "gunler":"Cumartesi-Pazar",
+                    "acilis": "10:00",
+                    "kapanis": "22:00",
+                    "kapali": false
+                }
+            ],
+            "koordinatlar": {
+                "enlem": "37.78334",
+                "boylam": "30.54530"
+            },
+            "yorumlar": [
+                {
+                    "yorumYapan": "Şevval Tuncay",
+                    "puan": "3",
+                    "tarih": "20 Ekim 2022",
+                    "yorumMetni": "Kahveleri ve pastaları güzel"
+                }
+            ]
+        } 
+
+    });
 }
 const yorumekle = function(req, res){
     res.render('yorumekle', { "title": "Yorum Sayfasi" });
