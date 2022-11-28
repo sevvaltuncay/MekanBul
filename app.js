315a25ab8a46fafd210549f82927b2a88094c193
Şevval Tuncay <sevvaltuncy@gmail.com>
2022-11-28 13:15:22 +0300

    sixth commit

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
 
 var app = express();
-app.use("/api", apiRouter);
 
 app.set('views', path.join(__dirname,'app_server' ,'views'));
 app.set('view engine', 'pug');
@@ -17,6 +16,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api", apiRouter);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
