b65b0234389e3d0a90c6f1582c4dc1b135557d34
Şevval Tuncay <sevvaltuncy@gmail.com>
2022-12-09 14:30:36 +0300

    eighth commit

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const axios = require("axios");
 
 var apiSecenekleri = {
-  sunucu: "https://mekanbul8.sevvaltuncay.repl.co",
+  sunucu: "https://mekanbul-1.sevvaltuncay.repl.co",
   apiYolu: "/api/mekanlar/",
 };
 var mesafeyiFormatla = function(mesafe) {
